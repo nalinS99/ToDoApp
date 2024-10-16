0ee90f14d4785c03bebba512d9c2f87be69002fe
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2024-10-16 12:50:55 +0530

    Modify the UI

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
-const express = require("express")
-const mongoose= require("mongoose")
-const cors = require("cors")
-const TodoModel = require("./Models/Todo")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const TodoModel = require("./Models/Todo");
 
-const app= express()
-app.use(cors())
-app.use(express.json())
-mongoose.connect("mongodb://localhost:27017/todos")
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose.connect("mongodb://localhost:27017/todos");
 
-app.post("/add",(req,res)=>{
-    const task= req.body.task;
-    TodoModel.create({
-        task:task
-    }).then(result => res.json(result)).catch(err => res.json(err) )
-} )
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  TodoModel.create({
+    task: task,
+  })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+app.get("/get", (req, res) => {
+    TodoModel.find()
+      .then((result) => res.json(result))
+      .catch((err) => res.json(err));
+  });
+  
 
-app.listen(3001 ,()=>{
-    console.log("server is running")
-}) 
-
+app.listen(3001, () => {
+  console.log("server is running");
+});
