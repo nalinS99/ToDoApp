d0d8ee4716bae8b34be8d1df31e84889f7c30999
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2024-10-22 17:02:21 +0530

    Final App

--- server/Models/Todo.js
+++ server/Models/Todo.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema({
-    task: {
-        type: String,
-        required: true // Ensures that task is provided
-    }
+  task: {
+    type: String,
+
+    required: true,
+  },
+  done: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const TodoModel = mongoose.model("Todo", TodoSchema);
